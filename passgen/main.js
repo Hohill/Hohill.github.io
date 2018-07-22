2af5ea86b530855b99d448f6c8d51fc9f335aca3
hohill <hohillyan@gmail.com>
2018-07-22 23:25:23 +0800

    完成生成随机字符串核心函数

---
+++ passgen/main.js
@@ -0,0 +1,34 @@
+
+function getRandomChar(chars) {
+    var i = Math.floor(Math.random() * chars.length);
+    return chars[i];
+}
+
+function getNoRepeatRandomChars(chars, size) {
+    if (chars.length < size) {
+        throw new Error("chars.length must greator than size when noRepeat is true");
+    }
+    var str = "";
+    for (var i = 0; i < size; i++) {
+        for (; ;) {
+            var c = getRandomChar(chars);
+            if (str.indexOf(c) === -1) {
+                str += c;
+                break;
+            }
+        }
+    }
+    return str;
+}
+
+function getRandomChars(chars, size) {
+    var str = "";
+    for (var i = 0; i < size; i++) {
+        str += getRandomChar(chars);
+    }
+    return str;
+}
+
+console.log(getNoRepeatRandomChars("abcdefg", 5));
+console.log(getRandomChars("abcdefg", 10));
+console.log(getRandomChars("abcdefg", 10));
