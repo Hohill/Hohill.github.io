91ce224c13be1493880bc3571203fa7802b42c4c
hohill <hohillyan@gmail.com>
2018-07-23 22:56:24 +0800

    generatePassword()重构

--- passgen/main.js
+++ passgen/main.js
@@ -9,8 +9,8 @@ function getRandomChar(chars) {
   return chars[i];
 }
 
-function getNoRepeatRandomChars(size, chars) {
-  if (chars.length < size) {
+function getNoRepeatRandomChars(size, chars, excludes) {
+  if (chars.length + excludes.length < size) {
     throw new Error(
       "chars.length must greator than size when noRepeat is true"
     );
@@ -19,7 +19,7 @@ function getNoRepeatRandomChars(size, chars) {
   for (var i = 0; i < size; i++) {
     for (;;) {
       var c = getRandomChar(chars);
-      if (str.indexOf(c) === -1) {
+      if (str.indexOf(c) === -1 && excludes.indexOf(c) === -1) {
         str += c;
         break;
       }
@@ -28,10 +28,21 @@ function getNoRepeatRandomChars(size, chars) {
   return str;
 }
 
-function generatePassword(size, firstCharts, restCharts) {
-  return (
-    getRandomChar(firstCharts) + getNoRepeatRandomChars(size - 1, restCharts)
-  );
+function generatePassword() {
+  var parts = Array.prototype.slice.call(arguments);
+  var str = "";
+  for (var i = 0; i < parts.length; i++) {
+    var size = parts[i][0];
+    var chars = parts[i][1];
+    str += getNoRepeatRandomChars(size, chars, str);
+  }
+  return str;
 }
 
-console.log(generatePassword(10, ALPHA, ALPHA + DIGIT + SPECIAL_SYMBOL));
+console.log(
+  generatePassword(
+    [2, UPPER_CASE_ALPHA + LOWER_CASE_ALPHA],
+    [5, LOWER_CASE_ALPHA + LOWER_CASE_ALPHA + DIGIT],
+    [1, SPECIAL_SYMBOL]
+  )
+);
