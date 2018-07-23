f6a8459a6f57723c712839f4cfa197ff96c1a0ea
hohill <hohillyan@gmail.com>
2018-07-23 22:16:29 +0800

    完成generatePassword核心代码

--- passgen/main.js
+++ passgen/main.js
@@ -1,9 +1,15 @@
+var DIGIT = "0123456789";
+var LOWER_CASE_ALPHA = "abcdefghijklmnopqrstuvwxyz";
+var UPPER_CASE_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var ALPHA = LOWER_CASE_ALPHA + UPPER_CASE_ALPHA;
+var SPECIAL_SYMBOL = "~!@#$%^&*?";
+
 function getRandomChar(chars) {
   var i = Math.floor(Math.random() * chars.length);
   return chars[i];
 }
 
-function getNoRepeatRandomChars(chars, size) {
+function getNoRepeatRandomChars(size, chars) {
   if (chars.length < size) {
     throw new Error(
       "chars.length must greator than size when noRepeat is true"
@@ -22,14 +28,10 @@ function getNoRepeatRandomChars(chars, size) {
   return str;
 }
 
-function getRandomChars(chars, size) {
-  var str = "";
-  for (var i = 0; i < size; i++) {
-    str += getRandomChar(chars);
-  }
-  return str;
+function generatePassword(size, firstCharts, restCharts) {
+  return (
+    getRandomChar(firstCharts) + getNoRepeatRandomChars(size - 1, restCharts)
+  );
 }
 
-console.log(getNoRepeatRandomChars("abcdefg", 5));
-console.log(getRandomChars("abcdefg", 10));
-console.log(getRandomChars("abcdefg", 10));
+console.log(generatePassword(10, ALPHA, ALPHA + DIGIT + SPECIAL_SYMBOL));
