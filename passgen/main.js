5030f8c5ba9984d249ffc8bb4ddef734a6ee2645
hohill <hohillyan@gmail.com>
2018-07-23 20:51:00 +0800

    reformat

--- passgen/main.js
+++ passgen/main.js
@@ -1,32 +1,33 @@
-
 function getRandomChar(chars) {
-    var i = Math.floor(Math.random() * chars.length);
-    return chars[i];
+  var i = Math.floor(Math.random() * chars.length);
+  return chars[i];
 }
 
 function getNoRepeatRandomChars(chars, size) {
-    if (chars.length < size) {
-        throw new Error("chars.length must greator than size when noRepeat is true");
+  if (chars.length < size) {
+    throw new Error(
+      "chars.length must greator than size when noRepeat is true"
+    );
+  }
+  var str = "";
+  for (var i = 0; i < size; i++) {
+    for (;;) {
+      var c = getRandomChar(chars);
+      if (str.indexOf(c) === -1) {
+        str += c;
+        break;
+      }
     }
-    var str = "";
-    for (var i = 0; i < size; i++) {
-        for (; ;) {
-            var c = getRandomChar(chars);
-            if (str.indexOf(c) === -1) {
-                str += c;
-                break;
-            }
-        }
-    }
-    return str;
+  }
+  return str;
 }
 
 function getRandomChars(chars, size) {
-    var str = "";
-    for (var i = 0; i < size; i++) {
-        str += getRandomChar(chars);
-    }
-    return str;
+  var str = "";
+  for (var i = 0; i < size; i++) {
+    str += getRandomChar(chars);
+  }
+  return str;
 }
 
 console.log(getNoRepeatRandomChars("abcdefg", 5));
