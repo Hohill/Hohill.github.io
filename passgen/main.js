aa44e8f736fb27b2f844839e825e02fe57f757d1
hohill <hohillyan@gmail.com>
2018-07-24 22:00:54 +0800

    完成核心功能

--- passgen/main.js
+++ passgen/main.js
@@ -28,20 +28,61 @@ function getNoRepeatRandomChars(size, chars, excludes) {
   return str;
 }
 
-function generatePassword() {
+function getRandomChars(size, chars) {
   var str = "";
-  for (var i = 0; i < arguments.length; i++) {
-    var size = arguments[i][0];
-    var chars = arguments[i][1];
-    str += getNoRepeatRandomChars(size, chars, str);
+  for (var i = 0; i < size; i++) {
+    str += getRandomChar(chars);
   }
   return str;
 }
 
-console.log(
-  generatePassword(
-    [2, UPPER_CASE_ALPHA + LOWER_CASE_ALPHA],
-    [5, LOWER_CASE_ALPHA + LOWER_CASE_ALPHA + DIGIT],
-    [1, SPECIAL_SYMBOL]
-  )
-);
+function generatePassword(fn) {
+  var str = "";
+  for (var i = 1; i < arguments.length; i++) {
+    var size = arguments[i][0];
+    var chars = arguments[i][1];
+    str += fn(size, chars, str);
+  }
+  return str;
+}
+
+function doGenerate() {
+  var size = Number(document.getElementById("size").value);
+  var first = "";
+  if (document.getElementById("first-lower").checked) {
+    first += LOWER_CASE_ALPHA;
+  }
+  if (document.getElementById("first-upper").checked) {
+    first += UPPER_CASE_ALPHA;
+  }
+  if (document.getElementById("first-digit").checked) {
+    first += DIGIT;
+  }
+  if (document.getElementById("first-symbol").checked) {
+    first += SPECIAL_SYMBOL;
+  }
+  var rest = "";
+  if (document.getElementById("rest-lower").checked) {
+    rest += LOWER_CASE_ALPHA;
+  }
+  if (document.getElementById("rest-upper").checked) {
+    rest += UPPER_CASE_ALPHA;
+  }
+  if (document.getElementById("rest-digit").checked) {
+    rest += DIGIT;
+  }
+  if (document.getElementById("rest-symbol").checked) {
+    rest += SPECIAL_SYMBOL;
+  }
+  var fn = getNoRepeatRandomChars;
+  var repeat = document.getElementsByName("repeat");
+  for (var i = 0; i < repeat.length; i++) {
+    if (repeat[i].checked) {
+      if (repeat[i].value === "yes") {
+        fn = getRandomChars;
+      }
+    }
+  }
+  var str = generatePassword(fn, [1, first], [size - 1, rest]);
+  document.getElementById("results").value = str;
+}
